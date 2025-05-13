3156c5c1e679fb9254cb57cee9968f3c3be7c0fe
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-05-13 08:10:59 +0000

    first commit

---
+++ create-metaflex-nft-collection.ts
@@ -0,0 +1,24 @@
+
+
+
+console.log("=======================================================================================")
+
+interface User{
+    name: string,
+    age:number
+}
+
+function main() {
+    const user: User = {
+        name:"Deep", age:95
+    }  
+
+    console.log("User is here : ",user)
+}
+main()
+
+
+
+
+
+console.log("=======================================================================================")
